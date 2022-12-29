e7717908444b2d1d81f13b8dd26113e185e40e0e
Drazox <68930610+drazoxXD@users.noreply.github.com>
2022-12-29 23:53:25 +0100

    NAGY HIBA FIX
    Területek nem töltenek be FIX!

--- client/territories.lua
+++ client/territories.lua
@@ -48,7 +48,7 @@ AddEventHandler("qb-gangs:client:updateblips", function(zone, winner)
 end)
 
 Citizen.CreateThread(function()
-    while loaded==true do 
+    while true do 
         Citizen.Wait(500)
         if PlayerGang.name ~= "none" or PlayerJob.name == "police" then
             local PlayerPed = PlayerPedId()
